357458394756fa3780238254232cd2ff7f77360b
Ravi3599 <ravikantdukre2018@gmail.com>
2022-03-08 16:20:09 +0530

    ADDED: created required components and routed them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
